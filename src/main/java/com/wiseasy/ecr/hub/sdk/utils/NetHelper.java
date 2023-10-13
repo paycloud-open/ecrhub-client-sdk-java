533b9a3420a19dcb7b9b94393b57cf35fe654030
jiangzj <jiangzhijun@wiseasy.com>
2023-10-13 16:25:59 +0800

    get mac address

--- src/main/java/com/wiseasy/ecr/hub/sdk/utils/NetHelper.java
+++ src/main/java/com/wiseasy/ecr/hub/sdk/utils/NetHelper.java
@@ -2,8 +2,10 @@ package com.wiseasy.ecr.hub.sdk.utils;
 
 import cn.hutool.core.net.NetUtil;
 
+import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.LinkedHashSet;
 
 public class NetHelper {
 
@@ -18,6 +20,27 @@ public class NetHelper {
     }
 
     public static String getLocalMacAddress() {
-        return NetUtil.getLocalMacAddress();
+        InetAddress address = getLocalhost();
+        if (address != null) {
+            return NetUtil.getMacAddress(address);
+        } else {
+            return NetUtil.getLocalMacAddress();
+        }
+    }
+
+    public static InetAddress getLocalhost() {
+        LinkedHashSet<InetAddress> addressList = NetUtil.localAddressList(address -> {
+            // 非loopback地址，指127.*.*.*的地址
+            return false == address.isLoopbackAddress()
+                    // 需为IPV4地址
+                    && address instanceof Inet4Address;
+        });
+        for (InetAddress address : addressList) {
+            if (address.isSiteLocalAddress()) {
+                // 本地地址，指10.0.0.0 ~ 10.255.255.255、172.16.0.0 ~ 172.31.255.255、192.168.0.0 ~ 192.168.255.255
+                return address;
+            }
+        }
+        return null;
     }
 }
